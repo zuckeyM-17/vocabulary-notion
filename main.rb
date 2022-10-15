9b88d763b079882ba1e16347da44d5cc2f4356cc
zuckeyM-17 <k.matsumura0314@gmail.com>
2022-10-16 00:20:28 +0900

    deepl の翻訳関数を作成

--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
-# coding: utf-8
+# frozen_string_literal: true
+
 require 'dotenv'
+require 'deepl'
 
 Dotenv.load
 
 def translate(english_word)
+  DeepL.configure do |config|
+    config.auth_key = ENV['DEEPL_API_KEY']
+    config.host = 'https://api-free.deepl.com'
+  end
+
+  translation = DeepL.translate english_word, nil, 'JA'
+  translation.text
 end
+
+puts translate('dog')
+puts translate('cat')
+
